22101c9a7cbace05bc7563ff3932e54a53842fd3
zannah-rain <zoe.pellatt.0@gmail.com>
2019-06-23 15:50:54 +0100

    Add a window to learnopengl

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -22,16 +22,32 @@ int main(int argc, char* argv[])
 		logger.log("GLFW couldn't initialize");
 		return -1;
 	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Creating the window
-	// std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window(glfwCreateWindow(640, 480, "WINDOW_TITLE", NULL, NULL), &glfwDestroyWindow);
-	GLFWwindow * window = glfwCreateWindow(640, 480, "BAL", NULL, NULL);
+	std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window(glfwCreateWindow(640, 480, "LearnOpenGL", NULL, NULL), &glfwDestroyWindow);
 	if (window == NULL)
 	{
 		logger.log("Window couldn't be created by GLFW");
+		glfwTerminate();
 		return 1;
 	}
 
+	// Set this window as our main context on the current thread
+	glfwMakeContextCurrent(window.get());
+
+	// Initialise GLAD before doing anything with openGL
+	// GLAD manages function pointers for openGL
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		logger.log("Failed to initialize GLAD");
+		glfwTerminate();
+		return 2;
+	}
+
 	// Cleanup before closing
+	glfwTerminate();
 	return 0;
 }
