5a8201efdffe4f096009420f848ad14850afcb48
zannah-rain <zoe.pellatt.0@gmail.com>
2019-06-23 19:54:47 +0100

    learnopengl completed "HelloWindow"

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -2,13 +2,22 @@
 #include "FileSystem.h"
 #include "Logger.h"
 // GLAD MUST BE INCLUDED BEFORE glfw
-#include <glad/glad.h>
+#include "glad/glad.h"
 #include "glfw3.h"
+
+// A callback function we'll use for changing the openGL viewport size
+void framebuffer_size_callback(GLFWwindow * window, int height, int width);
+
+// A function to handle inputs
+void processInput(GLFWwindow * window);
 
 int main(int argc, char* argv[])
 {
 	Logger logger;
 	FileSystem fileSystem;
+
+	unsigned int cWindowWidth = 640;
+	unsigned int cWindowHeight = 480;
 	
 	logger.log("Program started");
 	
@@ -27,7 +36,7 @@ int main(int argc, char* argv[])
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Creating the window
-	std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window(glfwCreateWindow(640, 480, "LearnOpenGL", NULL, NULL), &glfwDestroyWindow);
+	std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window(glfwCreateWindow(cWindowWidth, cWindowHeight, "LearnOpenGL", NULL, NULL), &glfwDestroyWindow);
 	if (window == NULL)
 	{
 		logger.log("Window couldn't be created by GLFW");
@@ -47,7 +56,45 @@ int main(int argc, char* argv[])
 		return 2;
 	}
 
+	// Set the initial dimensions of the output window for openGL
+	glViewport(0, 0, cWindowWidth, cWindowHeight);
+
+	glfwSetFramebufferSizeCallback(window.get(), framebuffer_size_callback);
+
+	// Set the "clear" color we'll overwrite previous frames with before drawing
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window.get()))
+	{
+		// Process inputs since last frame
+		processInput(window.get());
+
+		// Render stuff
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// We have one buffer for drawing to and one to send to the screen
+		glfwSwapBuffers(window.get());
+		glfwPollEvents();
+	}
+
 	// Cleanup before closing
 	glfwTerminate();
 	return 0;
+}
+
+// Create a callback function to keep the openGL viewport size synced with
+// the window size
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Handle inputs
+void processInput(GLFWwindow * window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
