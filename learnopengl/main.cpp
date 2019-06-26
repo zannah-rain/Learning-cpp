ff0e4bbe7742479aa6f60fb1cae475a1405a26f7
zannah-rain <zoe.pellatt.0@gmail.com>
2019-06-27 00:11:40 +0100

    Control colour with a uniform!

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -164,6 +164,13 @@ int main(int argc, char* argv[])
 		// Process inputs since last frame
 		processInput(window.get());
 
+		// Update the ourColor uniform
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUseProgram(shaderProgram);
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
 		// Render stuff
 		glClear(GL_COLOR_BUFFER_BIT);
 
--- learnopengl/rgbaFragmentShader.h
+++ learnopengl/rgbaFragmentShader.h
@@ -4,8 +4,10 @@ out vec4 FragColor;
 
 in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)
 
+uniform vec4 ourColor;
+
 void main()
 {
-	FragColor = vertexColor;
+	FragColor = ourColor;
 }
 )"
